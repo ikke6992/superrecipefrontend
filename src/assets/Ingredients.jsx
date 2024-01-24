578639a049c819a83290ea70f417ac016d9b051b
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-01-24 14:59:53 +0100

    added form without functionality which can be used to add ingredients to the database

--- src/assets/Ingredients.jsx
+++ src/assets/Ingredients.jsx
@@ -4,12 +4,18 @@ import { useEffect, useState } from "react";
 export default function Ingredients() {
 
     const [ingredients, setIngredients] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [units, setUnits] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
 
             const getIngredients = await axios.get("http://localhost:8080/api/ingredients/");
             setIngredients(getIngredients.data);
+            const getCategories = await axios.get("http://localhost:8080/api/categories/");
+            setCategories(getCategories.data);
+            const getUnits = await axios.get("http://localhost:8080/api/units/");
+            setUnits(getUnits.data);
         };
 
         fetchData();
@@ -19,10 +25,26 @@ export default function Ingredients() {
         console.log(ingredient);
         return (
             <li key={index}>
-                {ingredient.name}, Categorie: {ingredient.category.name}, Unit: {ingredient.unit}
+                {ingredient.name}, Categorie: {ingredient.category.name}, Unit: {ingredient.unit.name}
             </li>
         )
     });
+
+    const submitRecipe = (e) => {
+        e.preventDefault();
+    }
+
+    const categoryOptions = categories.map((category, index) => {
+        return (
+            <option key={index} value={category.name}>{category.name}</option>
+        )
+    })
+
+    const unitOptions = units.map((unit, index) => {
+        return (
+            <option key={index} value={unit.name}>{unit.name}</option>
+        )
+    })
 
     return (
         <>
@@ -30,6 +52,24 @@ export default function Ingredients() {
             <ul>
                 {basicInfo}
             </ul>
+            <form onSubmit={submitRecipe}>
+                <label>
+                    Naam: <input type='text' placeholder='ingredient' />
+                </label><br />
+                <label>
+                    Categorie: 
+                    <select>
+                        {categoryOptions}
+                    </select>
+                </label><br />
+                <label>
+                    Unit: 
+                    <select>
+                        {unitOptions}
+                    </select>
+                </label><br />
+                <button type='submit'>Submit</button>
+            </form>
         </>
     );
 }
